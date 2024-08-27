9299a66a330d4be071a926bb72ed31a1d79a03b2
killerapp <vaskin@gmail.com>
2024-08-27 15:27:16 -0500

    feat: Add Prometheus metrics for voicebot sessions

--- app__init__.py
+++
@@ -1,5 +0,0 @@
-from prometheus_client import Counter, Gauge
-
-# Create Prometheus metrics
-SESSION_COUNTER = Counter('voicebot_session_count', 'Number of sessions started')
-SESSION_GAUGE = Gauge('voicebot_active_sessions', 'Current number of active sessions')
